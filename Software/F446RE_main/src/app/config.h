8a7478c2e0596abf2195930da419703433c9b376
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-01-27 19:42:29 +0900

    ライン送信UARTインターバル変更

--- Software/F446RE_main/src/app/config.h
+++ Software/F446RE_main/src/app/config.h
@@ -4,7 +4,7 @@
 #define MAIN_FREQ 5000  // Hz
 #define PERIOD_US (int)((1.0f / MAIN_FREQ) * 1000000)
 
-#define LINE_SEND_FREQ 1000  // Hz
+#define LINE_SEND_FREQ 100  // Hz
 #define LINE_SEND_PERIOD_US (int)((1.0f / LINE_SEND_FREQ) * 1000000)
 
 #endif
