fa838d38a4b05fc8d931cfdc05b1319d4789e8e4
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-02-07 08:53:49 +0900

    UART修正

--- Software/ATmega328P_ui/include/mode/line.h
+++ Software/ATmega328P_ui/include/mode/line.h
@@ -7,22 +7,51 @@ int16_t line_dir;
 int16_t line_inside_dir;
 uint8_t line_interval;
 bool is_on_line;
-bool is_left_line;
-bool is_right_line;
+bool is_leftside;
+bool is_rightside;
 
 void Line() {
-      if (Serial.available() > 0) {
-            if (Serial.read() == 0xFF) {
-                  uint8_t bool_data;
-                  line_dir = Serial.read() * 2 - 180;
-                  line_inside_dir = Serial.read() * 2 - 180;
-                  line_interval = Serial.read();
-                  bool_data = Serial.read();
-                  is_on_line = (bool_data >> 2) & 1;
-                  is_left_line = (bool_data >> 1) & 1;
-                  is_right_line = bool_data & 1;
+      // if (Serial.available() > 0) {
+      //       if (Serial.read() == 0xFF) {
+      //             uint8_t bool_data;
+      //             line_dir = Serial.read() * 2 - 180;
+      //             line_inside_dir = Serial.read() * 2 - 180;
+      //             line_interval = Serial.read();
+      //             bool_data = Serial.read();
+      //             is_on_line = (bool_data >> 2) & 1;
+      //             is_left_line = (bool_data >> 1) & 1;
+      //             is_right_line = bool_data & 1;
 
-                  while (Serial.available() > 0) Serial.read();
+      //             while (Serial.available() > 0) Serial.read();
+      //       }
+      // }
+      static const uint8_t HEADER = 0xFF;   // ヘッダ
+      static const uint8_t FOOTER = 0xAA;   // ヘッダ
+      static const uint8_t data_size = 4;   // データのサイズ
+      static uint8_t index = 0;             // 受信したデータのインデックスカウンター
+      static uint8_t recv_data[data_size];  // 受信したデータ
+      static uint8_t recv_byte;
+      while (Serial.available()) {
+            recv_byte = Serial.read();
+            if (index == 0) {
+                  if (recv_byte == HEADER) {
+                        index++;
+                  } else {
+                        index = 0;
+                  }
+            } else if (index == (data_size + 1)) {
+                  if (recv_byte == FOOTER) {
+                        line_dir = recv_data[0] * 2 - 180;
+                        line_inside_dir = recv_data[1] * 2 - 180;
+                        line_interval = recv_data[2];
+                        is_on_line = (recv_data[3] >> 2) & 1;
+                        is_leftside = (recv_data[3] >> 1) & 1;
+                        is_rightside = recv_data[3] & 1;
+                  }
+                  index = 0;
+            } else {
+                  recv_data[index - 1] = recv_byte;
+                  index++;
             }
       }
 
@@ -44,8 +73,8 @@ void Line() {
       }
 
       if (is_on_line == 1) led.SetDegree(line_inside_dir, 0, 1, 0);
-      if (is_left_line) led.SetPixelColor(12, 0, 0, 255);
-      if (is_right_line) led.SetPixelColor(4, 0, 0, 255);
+      if (is_leftside) led.SetPixelColor(12, 0, 0, 255);
+      if (is_rightside) led.SetPixelColor(4, 0, 0, 255);
 }
 
 #endif
