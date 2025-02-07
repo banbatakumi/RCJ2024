ba819f4c276bad17298c02a8907184569cc63626
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-02-07 14:44:50 +0900

    ボールを追ってカニ歩きした

--- Software/F446RE_main/src/mode/defence.cpp
+++ Software/F446RE_main/src/mode/defence.cpp
@@ -1,4 +1,9 @@
 #include "mode.hpp"
 
 void Mode::Defence() {
+      if (robot->info.Cam.ball_dir > 45) {
+            robot->motor.Drive(90, abs(45 - robot->info.Cam.ball_dir) / 45.000);
+      } else {
+            robot->motor.Drive(-90, abs(45 - robot->info.Cam.ball_dir) / 45.000);
+      }
 }
